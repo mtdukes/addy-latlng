c4b4e4a179aec8342bb9240f86b621657520b9a3
mtdukes <tyler.dukes@gmail.com>
2015-05-22 16:27:46 -0400

    uploading script

---
+++ README.md
@@ -0,0 +1,23 @@
+#addy-latlng
+Simple script to read in a CSV with the column "full_address" and append latitudes, longitudes and confidence scores to a new CSV.
+
+##Requirements
+To run zips2fips, you'll need to import the [Data Science Toolkit](http://www.datasciencetoolkit.org/developerdocs#street2coordinates).
+
+```
+pip install dstk
+```
+
+You'll also need an existing CSV with addresses in a column marked "full_address." The script will search for this column before running geolocation.
+
+##Running it
+Run addy-latlng by passing in the path to your CSV as an argument.
+
+```
+python addy-latlng.py SOURCE_DATA.csv
+```
+
+The file (e.g. SOURCE_DATA_latlng.csv) will be stored in the directory in which you run the script.
+
+##Like it?
+Check out a related script, [zips2fips](https://github.com/mtdukes/zips2fips), which helps convert zip codes to FIP codes for mapping purposes.
---
+++ addy-latlng.py
@@ -0,0 +1,58 @@
+import dstk
+import csv, argparse, re
+
+def get_latlng(csv_path):
+	#initialize csv file
+	with open(csv_path, 'rU') as csv_input:
+		#strip out directory, if it exists
+		loc = re.sub(r'(^.*\/)','',csv_path)
+		with open(loc+"_latlng",'w') as csv_output:
+			writer = csv.writer(csv_output)
+			reader = csv.reader(csv_input)
+
+			#initialize DSTK
+			d = dstk.DSTK()
+
+			#get location of full_address column
+			row = reader.next()
+			address_position = row.index('full_address')
+
+			#add columns
+			row.append('latitude')
+			row.append('longitude')
+			row.append('confidence')
+			writer.writerow(row)
+
+			print row
+			for item in reader:
+				#clear out instances of suite, room, office, etc.
+				clean_address = re.sub(r'(( SUITE.*?)|( ROOM.*?)|( OFFICE.*?)),',',',item[address_position])
+				#find 'full_address' in CSV file
+				raw = d.street2coordinates(clean_address)
+				#raw = d.street2coordinates(item[address_position])
+				print raw
+				for r in raw:
+					r_dict = raw[r]
+					#add 'latitude' to csv
+					try:
+						item.append(r_dict['latitude'])
+					except TypeError:
+						item.append('')
+					#add 'longitude' to csv
+					try:
+						item.append(r_dict['longitude'])
+					except TypeError:
+						item.append('')
+					#add 'confidence' to csv
+					try:
+						item.append(str(r_dict['confidence']))
+					except TypeError:
+						item.append('')
+				writer.writerow(item)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Convert full address to FIPS code. Eventually: CSV path')
+	parser.add_argument('path',help='Enter the full address')
+	args = parser.parse_args()
+
+	get_latlng(args.path)
